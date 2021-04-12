0c33a77e10be0ced8d0c33325d742baf74cadfff
ronak4779 <81635920+ronak4779@users.noreply.github.com>
2021-04-12 15:39:49 +0530

    Add files via upload
    real boiler

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# real-boiler
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ boiler real.js
@@ -0,0 +1,45 @@
+var wall , thickness;
+var speed , weight , bullet;
+
+function setup() {
+  createCanvas(1600,400);
+  speed = random(223 , 321)
+  weight = random(30 , 52)
+  thickness=random(22 , 83)
+   wall = createSprite(1200 , 200 , thickness , height/2);
+  
+}
+
+function draw() {
+  background(255,255,255);  
+
+if(hasCollided(bullet , wall))
+{
+  bullet.velocityX = 0;
+  var damage = 0.5 * weight * speed* speed/(thickness *thickness * thickness);
+
+}
+if(damage>10)
+{
+    wall.shapeColor=color(255 , 0 , 0);
+}
+
+if(damage<10)
+{
+    wall.shapeColor(0 , 255 , 0);
+}
+
+
+  drawSprites();
+}
+
+function hasCollided(bullet , wall)
+{
+    bulletRightEdge=bullet.x + bullet.width;
+    wallLeftEdge=wall.x;
+    if(bulletRightEdge>=wallLeftEdge)
+        {
+            return true;
+        }
+            return false;
+}
